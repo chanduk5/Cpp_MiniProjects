e7532f925ea41b7cc78566b45453df0f8d2f38dd
chandu <kurapatichandu535@gmail.com>
2020-01-21 11:41:56 +0100

    3Dball.Cpp added and 3Dball.c deleted Code added into 3Dball.cpp and 3Dball.h

--- 3Dball/3Dball.c
+++
@@ -1,8 +0,0 @@
-/*
- * GLUT 3D shapes
- *
- * Written by Chandu Kurapati
- *
- * This program is show the 3D balls with GLUT.
- *
- */
--- 3Dball/3Dball.h
+++ 3Dball/3Dball.h
@@ -6,23 +6,82 @@
  * This program is show the 3D balls with GLUT.
  *
  */
-#ifndef _3DBALL_H
-#define _3DBALL_H
 
+#ifndef _3DBALL_H_
+#define _3DBALL_H_
 
+#include<windows.h>
 
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
+#include <cmath>
+#include <stdlib.h>
 
+// A camera.
+class Camera {
+  double theta;      // determines the x and z positions
+  double y;          // the current y position
+  double dTheta;     // increment in theta for swinging the camera around
+  double dy;         // increment in y for moving the camera up/down
+public:
+  Camera(): theta(0), y(3), dTheta(0.04), dy(0.2) {}
+  double getX() {return 10 * cos(theta);}
+  double getY() {return y;}
+  double getZ() {return 10 * sin(theta);}
+  void moveRight() {theta += dTheta;}
+  void moveLeft() {theta -= dTheta;}
+  void moveUp() {y += dy;}
+  void moveDown() {if (y > dy) y -= dy;}
+};
 
+// A ball.
+class Ball {
+  double radius;
+  GLfloat* color;
+  double maximumHeight;
+  double x;
+  double y;
+  double z;
+  int direction;
+public:
+  Ball(double r, GLfloat* c, double h, double x, double z):
+      radius(r), color(c), maximumHeight(h), direction(-1),
+      y(h), x(x), z(z) {
+  }
+  void update() {
+    y += direction * 0.05;
+    if (y > maximumHeight) {
+      y = maximumHeight; direction = -1;
+    } else if (y < radius) {
+      y = radius; direction = 1;
+    }
+    glPushMatrix();
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
+    glTranslated(x, y, z);
+    glutSolidSphere(radius, 30, 30);
+    glPopMatrix();
+  }
+};
 
+// A checkerboard.
+class Checkerboard {
+  int displayListId;
+  int width;
+  int depth;
+public:
+  Checkerboard(int width, int depth): width(width), depth(depth) {}
+  double centerx() {return width / 2;}
+  double centerz() {return depth / 2;}
 
+  void create(void);
+  void draw(void);
+};
 
-
-
-
-
-
-
-
+/* Display 3D balls */
+void glut3Dball(void);
 
 #endif // _3DBALL_H
