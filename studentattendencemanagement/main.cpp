815d8fcfce859ab3a2f344c3dec3212e5d5e154e
chandu <kurapatichandu535@gmail.com>
2020-01-23 10:47:03 +0100

    PackMan game is added

--- studentattendencemanagement/main.cpp
+++ studentattendencemanagement/main.cpp
@@ -1,19 +1,34 @@
 #include <QApplication>
 #include <QFont>
 #include <QPushButton>
+#include <QtWidgets>
+#include <QMainWindow>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    QPushButton quit("Quit");
+    QWidget * window = new QWidget;
+    window->resize(640, 480);
+    window->setWindowTitle(QApplication::translate("Studentattedence", "StudentAttendenceManagement"));
 
-    quit.resize(75, 30);
-    quit.setFont(QFont("Times", 18, QFont::Bold));
+    QVBoxLayout *vlay = new QVBoxLayout(window);
+    QPushButton *btn1 = new QPushButton("Admin");
+    vlay->addWidget(btn1);
+    QPushButton *btn2 = new QPushButton("Student");
+    vlay->addWidget(btn2);
+    window->setLayout(vlay);
+    window->show();
 
-    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
 
-    quit.show();
+    //QPushButton quit("Quit");
+
+    //quit.resize(75, 30);
+    //quit.setFont(QFont("Times", 18, QFont::Bold));
+
+    //QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
+
+    //quit.show();
 
     return app.exec();
 }
